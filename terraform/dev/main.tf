be83d3a38daf366cd1fb46ee2f2d0fa9489c1788
arsh-e-noor <arshkhan4877@gmail.com>
2026-06-26 17:30:56 +0500

    Add Terraform Jenkins EC2 module

---
+++ terraform/dev/main.tf
@@ -0,0 +1,12 @@
+module "jenkins_ec2" {
+  source = "../modules/jenkins-ec2"
+
+  instance_name = "jenkins-server"
+  instance_type = "t3.medium"
+
+  ami_id    = "ami-001e7cc215773c7fb"
+  key_name  = "capstone-key-v2"
+
+  vpc_id    = "vpc-03e7cf094cb107161"
+  subnet_id = "subnet-0c495c93450cfc1bb"
+}
